a1133e16db744622ce6b976af61317ec2bc2a3a0
chappy <s1613099@u.tsukuba.ac.jp>
2016-10-26 23:18:17 +0900

    update

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 $(function(){
 	var wid = window.innerWidth;
-	if( wid < 1000 ){
+	if( wid > 1000 ){
 		$('img').each(function(){
-			$(this).attr("src",$(this).attr("src").replace('_pc', '_sp'));
+			$(this).attr("src",$(this).attr("src").replace('_sp', '_pc'));
 		});
 	}
+	$('img').each(function(){
+		$(this).attr("src",$(this).attr("src")).css('visibility', 'visible');
+	});
 });
